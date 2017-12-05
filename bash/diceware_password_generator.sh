92dd13d3e2df7dea8c97765d3a7fa8b11956a247
marcobompani <marcobompani91@gmail.com>
2017-12-05 10:58:48 +0100

    Modify the help message

---
+++ bash/diceware_password_generator.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+DIR=$(dirname $0)
+LAN=en
+N=8
+numberflag=false
+languageflag=false
+for par in $@; do
+	if [ "$par" = -h ] || [ "$par" = --help ]; then
+		echo "Usage: `basename $0` [-h] [-l LANG] [-w WORDS]"
+		echo
+		echo "-h, --help	show this help and exit"
+		echo "-l, --lang	set the language of the list to use (default en)"
+		echo "-w, --words	set the password length in words (default 8)"
+		exit
+	fi
+	if [ "$numberflag" = true ]
+		then
+		N=$par
+		numberflag=false
+	fi
+	
+	if [ "$par" = --words ] || [ "$par" = -w ]
+		then
+		numberflag=true
+	fi
+	if [ "$languageflag" = true ]
+		then
+		LAN=$par
+		languageflag=false
+	fi
+	
+	if [ "$par" = --lang ] || [ "$par" = -l ]
+		then
+		languageflag=true
+	fi
+done	
+for ((i=1; i<=$N;i++)); do
+	NUMBER=$(cat /dev/urandom | tr -cd '1-6' | head -c 5)
+	WORD=$(awk -v n="$NUMBER" '$1~n{print $2}' word_list/word_list_$LAN.txt)
+	PASSWORD="$PASSWORD $WORD"
+done	
+echo $PASSWORD
