5150ece0d8a4988632fb01dae133093de44874bf
Marco Bompani <marcobompani91@gmail.com>
2019-09-28 17:55:58 +0200

    Style improvement

--- python/diceware_password_generator.py
+++ python/diceware_password_generator.py
@@ -1,6 +1,7 @@
 import argparse
 from random import SystemRandom
 
+
 def extractWord(code):
     with open('word_list/word_list_{}.txt'.format(args.lang), 'r') as f:
         for line in f:
@@ -8,9 +9,13 @@ def extractWord(code):
             if line.startswith(code):
                 return line.split()[1]
 
+
 parser = argparse.ArgumentParser()
-parser.add_argument('-l', '--lang', default='en', help='set the language of the list to use (default en)')
-parser.add_argument('-w', '--words', type=int, default=8, help='set the password length in words (default 8)')
+parser.add_argument('-l', '--lang', default='en', help='set the language of'
+                                                       'the list to use ('
+                                                       'default en)')
+parser.add_argument('-w', '--words', type=int, default=8, help='set the '
+                    'password length in words (default 8)')
 
 args = parser.parse_args()
 
@@ -18,8 +23,8 @@ password = ''
 cryptogen = SystemRandom()
 
 for i in range(0, args.words):
-    num = [cryptogen.randrange(1,7) for p in range(0,5)]
+    num = [cryptogen.randrange(1, 7) for p in range(0, 5)]
     code = ''.join(str(n) for n in num)
     password = password + ' ' + extractWord(code)
 
-print password.strip()
+print(password.strip())
